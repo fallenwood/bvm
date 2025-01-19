720ed4d341228cadadce95a86f2c891443d7feaf
fallenwood <fallenwood.y@outlook.com>
2025-01-19 12:23:20 +0800

    create directory

--- bvm/Utils/TarGzFile.cs
+++ bvm/Utils/TarGzFile.cs
@@ -5,6 +5,10 @@ using System.IO.Compression;
 
 public static class TarGzFile {
   public static void ExtractToDirectory(string filename, string outputDir) {
+    if (!Directory.Exists(outputDir)) {
+      Directory.CreateDirectory(outputDir);
+    }
+
     using var fileStream = File.OpenRead(filename);
     using var gzipStream = new GZipStream(fileStream, CompressionMode.Decompress);
     TarFile.ExtractToDirectory(gzipStream, outputDir, true);
